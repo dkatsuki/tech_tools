ec4df854882c0f121b0dd379260295bf49f6de0f
dkatsuki <oniku.tabetai813@gmail.com>
2022-07-03 21:38:04 +0900

    unicorn.rb

---
+++ config/unicorn.rb
@@ -0,0 +1,10 @@
+rails_root = File.expand_path('../../', __FILE__)
+
+worker_processes 4
+timeout 40
+preload_app true
+
+listen 			"#{rails_root}/tmp/unicorn.sock"
+pid 				"#{rails_root}/tmp/unicorn.pid"
+stderr_path "#{rails_root}/log/unicorn_error.log"
+stdout_path "#{rails_root}/log/unicorn.log"
