fe3eb1a3ecdc7639c7f170070914dc470fb31a94
dkatsuki <oniku.tabetai813@gmail.com>
2022-07-12 12:15:28 +0900

    make tool_pages/index

--- app/controllers/tool_pages_controller.rb
+++ app/controllers/tool_pages_controller.rb
@@ -1,2 +1,4 @@
 class ToolPagesController < ApplicationController
+	def index
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'application#health_check'
-  resources :tool_page
+  resources :tool_pages
 end
