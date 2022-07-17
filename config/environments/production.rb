2a9df7063b81bebcc3c0a9b756d43209285d7c00
dkatsuki <oniku.tabetai813@gmail.com>
2022-07-18 02:24:10 +0900

    update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.logger = ActiveSupport::Logger.new("log/production.log")
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
