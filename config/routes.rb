b4d91356581798e70545f777a0094184f98f7d3e
dkatsuki <oniku.tabetai813@gmail.com>
2022-07-22 01:34:47 +0900

    route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'tech_tools/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root to: 'application#health_check'
+  root to: 'tool_pages#index'
   resources :tool_pages
 end
